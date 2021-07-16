29eaa45cf09488b1331bd1c6f41771fe1fd9aa51
wannapornM <wannaporndangkum@gmail.com>
2021-07-17 03:51:29 +0700

    add function buildJobDropdown unfinished

--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
+require('dotenv').config()
 import './main.scss'
+
+document.addEventListener('DOMContentLoaded', function() {
+    let heroJobUrl = process.env.API_HOST + "/hero_jobs"
+    fetch(heroJobUrl, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    }).then(resp => resp.json())
+      .then(data => {
+          let jobWrapper = document.getElementById('job-wrapper')
+          if(jobWrapper == null) {return}
+
+          console.log(data)
+          buildJobDropdown(jobWrapper, data)
+      })
+})
+
+function buildJobDropdown(targetDom, data) {
+    targetDom.insertAdjacentHTML('afterbegin', `
+        <select id="jobs" name="hero[job]">
+            ${data.jobs.map(item => {return `<option value=${item}>${item}</option`})}
+            <option value=""></option>
+        </select
+    `)
+}
