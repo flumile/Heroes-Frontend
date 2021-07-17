649058f6937f33a4c17a136dd66ea4af44cb5abb
flumile <millionmiles17@hotmail.com>
2021-07-17 15:23:55 +0700

    just add hero_details branch

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
+require('dotenv').config()
 import './main.scss'
+
+document.addEventListener('DOMContentLoaded', function() {
+    let listHeroesDom = document.getElementById('each-hero')
+    let formHero = document.querySelector("form")
+    console.log('hello')
+})
