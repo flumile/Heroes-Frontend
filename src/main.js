53ef7d63b52a42ad32e7d731b8436ef284561369
muk-thapanee <muk356@gmail.com>
2021-07-21 15:41:19 +0700

    edit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 import './main.scss'
 
 document.addEventListener('DOMContentLoaded', function() {
+    let formHero = document.querySelector("form")
+    let btnSubmitHero = document.getElementById('btn-submit-hero')
+    
     let heroJobUrl = process.env.API_HOST + "/hero_jobs"
     fetch(heroJobUrl, {
         method: "GET",
@@ -14,15 +17,57 @@ document.addEventListener('DOMContentLoaded', function() {
           let jobWrapper = document.getElementById('job-wrapper')
           if(jobWrapper == null) {return}
 
-          console.log(data)
           buildJobDropdown(jobWrapper, data)
       })
-})
+      btnSubmitHero.onclick=()=>{
+        console.log("WOW")
+        createHero()
+    }
+    function createHero(){
+        let name = formHero.querySelector('#name').value
+        let job = formHero.querySelector('#job-wrapper').value
+        let image = formHero.querySelector('#image').files[0]
+        
+        let formData = new FormData()
+        formData.append('hero[name]',name)
+        formData.append('hero[job]',job)
+        formData.append('hero[image]',image)
+        console.log (formData) 
+        
+        let createHeroUrl = url
+        fetch(createHeroUrl, {
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json',
+                'Authorization': process.env.API_CREDENTIAL,
+            },
+            body: formData,
+            mode: 'cors'
+        })
+        .then(resp => resp.json())
+        .then(data =>{
 
-function buildJobDropdown(targetDom, data) {
-    targetDom.insertAdjacentHTML('afterbegin', 
-        `<select id="jobs" name="hero[job]">
-            ${data.jobs.map(item => `<option value=${item}>${item}</option>`)}
-        </select>
-    `)
-}
+            insertNewHero(listHeroesDom, data)
+        })
+    }
+})
+    function insertNewHero(heroList, hero){
+        let htmlStr = `
+        <div class="hero">
+            <a href="" class="hero-name">${hero.name}</a>
+            <div>${hero.level}</div>
+            <div>${hero.hp}</div>
+            <div>${hero.mp}</div>
+            <div>${hero.job}</div>
+        </div>
+        `
+        heroList.insertAdjacentHTML('afterbrgin', htmlStr)
+    } 
+
+    function buildJobDropdown(targetDom, data) {
+        targetDom.insertAdjacentHTML('afterbegin', 
+            `<select id="jobs" name="hero[job]">
+                ${data.jobs.map(item => `<option value=${item}>${item}</option>`)}
+            </select>
+        `)
+    }
